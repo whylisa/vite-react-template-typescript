d6086b5c0f0f319ca623bedbde61f25089b211be
whylisa <haoyue.wang@proinnova.com.cn>
2021-08-02 14:46:30 +0800

    fix:更新文档

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+    proxy: {
+      "/XXX": {
+        target: "https://XXX",
+        changeOrigin: true,
+        cookieDomainRewrite: "",
+        secure: false,
+      },
+    },
+  },
   plugins: [reactRefresh()],
 });
